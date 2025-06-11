fa2e444b67a4590a287457a558a947a05b1c78c2
Maksim Malov <malov-2005@bk.ru>
2025-06-11 20:53:55 +0300

    Fix(app.py): added blank lines

--- back/app.py
+++ back/app.py
@@ -12,13 +12,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
+
 @app.get("/ping/")
 def read_ping():
     return {"message": "Pong"}
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
