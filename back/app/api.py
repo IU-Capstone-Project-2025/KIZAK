91747da1e2209a608a20d41ec50288252bab4d64
Maksim Malov <malov-2005@bk.ru>
2025-06-14 14:40:53 +0300

    Fixes

--- back/app/api.py
+++ back/app/api.py
@@ -1,9 +1,9 @@
 from flask_restful import Api
 
 from .routes.cv import CVAPI
+from .routes.homepage import HomepageAPI
 from .routes.roadmap import RoadmapAPI
 from .routes.user import UserAPI
-from .routes.homepage import HomepageAPI
 
 
 def init_routes(app):
