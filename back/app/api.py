2ce78626f594bad7b4fa63d1a2ae46d5e42a16cf
Maksim Malov <malov-2005@bk.ru>
2025-06-14 14:35:47 +0300

    Fixes

--- back/app/api.py
+++ back/app/api.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from .routes.cv import CVAPI
 from .routes.roadmap import RoadmapAPI
 from .routes.user import UserAPI
+from .routes.homepage import HomepageAPI
 
 
 def init_routes(app):
@@ -11,3 +12,4 @@ def init_routes(app):
     api.add_resource(UserAPI, "/user", "/user/<int:user_id>")
     api.add_resource(RoadmapAPI, "/it roadmap")
     api.add_resource(CVAPI, "/cv")
+    api.add_resource(HomepageAPI, "/")
