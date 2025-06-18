59df67ce385eb790bd2e14a4323af5fad38d949b
1sarmatt <lutfullin.sarmat@gmail.com>
2025-06-18 15:21:53 +0300

    temp

--- front/app/(auth)/log-in/page.tsx
+++ front/app/(auth)/log-in/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
@@ -15,7 +16,6 @@ const Login: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
       <div className="w-full max-w-sm space-y-6 bg-white rounded ">
-        {/* Logo */}
         <div className="flex justify-center mb-4">
           <div className="w-16 h-16 rounded-full flex items-center justify-center">
             <div className="w-12 h-12  rounded-full relative overflow-hidden">
@@ -24,14 +24,12 @@ const Login: React.FC = () => {
           </div>
         </div>
 
-        {/* Title */}
         <h2 className="text-center text-lg font-medium text-ui-dark">Log in to your account</h2>
 
-        {/* Form */}
         <form className="space-y-4 flex-center flex-col">
           <input
-            type="email"
-            placeholder="Enter your email address..."
+            type="login"
+            placeholder="Enter your login..."
             className="h-[50px] placeholder: text-ui-muted w-100 px-4 py-2 border rounded-sm focus:outline-none focus:ring  outline:none border-ui-border"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -46,7 +44,7 @@ const Login: React.FC = () => {
           <button
             type="submit"
             disabled={!isValid}
-            className={`h-[50px] w-100 py-2 bg-brand-primary/80 hover:bg-brand-primary text-white font-semibold rounded-md transition-all duration-300 ${
+            className={`h-[50px] w-100 py-2  hover:bg-brand-primary text-white font-semibold rounded-md transition-all duration-300 ${
               isValid
                 ? 'bg-brand-primary hover:bg-brand-primary/90'
                 : 'bg-brand-primary/80 cursor-not-allowed'
@@ -56,11 +54,10 @@ const Login: React.FC = () => {
           </button>
         </form>
 
-        {/* Footer links */}
         <div className="flex justify-between text-xs text-ui-muted w-full mt-4">
-          <a href="#" className="hover:underline">
-            or Sign up
-          </a>
+        <Link href="/sign-up" className="hover:underline">
+  or Sign up
+</Link>
           <a href="#" className="hover:underline">
             Forgot your password?
           </a>
