b3d95f1661b4b9f3bbeda178c7202fa8ba92d576
LowIQCoder <marselberheev@mail.ru>
2025-06-21 01:08:21 +0300

    Some fixes

--- back/routers/resource.py
+++ back/routers/resource.py
@@ -13,22 +13,29 @@ router = APIRouter()
 
 
 @router.get(
-    "/resource/{res_id}", response_model=ResourceResponse, tags=["Resource"]
+    "/resource/{res_id}", response_model=ResourceResponse, tags=["Resource"],
+    description="Gets a resource from database based on UUID"
 )
 async def get_resource(res_id: UUID):
     return await retrieve_resource(res_id)
 
 
-@router.post("/resource/", response_model=ResourceResponse, tags=["Resource"])
+@router.post(
+    "/resource/", response_model=ResourceResponse, tags=["Resource"]
+)
 async def post_resource(res: ResourceCreate):
     return await create_resource(res)
 
 
-@router.put("/resource/", response_model=ResourceResponse, tags=["Resource"])
+@router.put(
+    "/resource/", response_model=ResourceResponse, tags=["Resource"]
+)
 async def put_resource(res: ResourceUpdate):
     return await update_resource(res)
 
 
-@router.delete("/resource/{res_id}", tags=["Resource"])
+@router.delete(
+    "/resource/{res_id}", tags=["Resource"]
+)
 async def delete_resource(res_id: UUID):
     return await remove_resource(res_id)
