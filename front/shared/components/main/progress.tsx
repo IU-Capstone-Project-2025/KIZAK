cee961e52eff456f1c42c7e8689d1c7a9cb1d681
1sarmatt <150796175+1sarmatt@users.noreply.github.com>
2025-07-10 23:57:16 +0300

    fix for adaptive

--- front/shared/components/main/progress.tsx
+++ front/shared/components/main/progress.tsx
@@ -17,15 +17,15 @@ export const MainProgress: React.FC<Props> = ({
     <div
       className={`flex flex-col rounded-xl border shadow-sm border-ui-border ${className}`}
     >
-      <h2 className="text-ui-dark text-md w-full pl-3 py-2 border-b border-ui-border">
+      <h2 className="text-ui-dark text-sm w-full pl-2 py-0.5 border-b border-ui-border">
         Progress
       </h2>
-      <div className="flex flex-col items-center justify-center flex-1 p-4">
-        <ProgressCircle progress={progress} size={300} strokeWidth={16} />
-        <p className="text-2xl mt-10 mb-4 text-center">You're almost done!</p>
+      <div className="flex flex-col items-center justify-center flex-1 p-0.5 overflow-auto">
+        <ProgressCircle progress={progress} size={220} strokeWidth={6} />
+        <p className="text-base mt-1 mb-0.5 text-center">You're almost done!</p>
         <TransitionLink
           delay={2000}
-          className="underline text-ui-muted text-xl"
+          className="underline text-ui-muted text-sm"
           href={`/roadmap/${userId}`}
         >
           Let's continue
