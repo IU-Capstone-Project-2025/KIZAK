510c5229253836856d0e3e98091fe2a6610503c2
1sarmatt <150796175+1sarmatt@users.noreply.github.com>
2025-07-10 23:59:39 +0300

    add adaptive

--- front/shared/components/main/tasks.tsx
+++ front/shared/components/main/tasks.tsx
@@ -17,7 +17,7 @@ export const MainTasks: React.FC<Props> = ({
     <div
       className={`rounded-xl border flex flex-col shadow-sm border-ui-border ${className}`}
     >
-      <h2 className="text-ui-dark text-md w-full pl-3 py-2 border-b border-ui-border">
+      <h2 className="text-ui-dark text-sm w-full pl-2 py-0.5 border-b border-ui-border">
         Last opened
       </h2>
       <div className="flex-1 flex flex-col gap-y-1 py-2">
