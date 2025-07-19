0c22032a004e73341150c021bd32357c47eecaa7
netimaaa <t.farizunov@innopolis.university>
2025-07-19 20:47:59 +0300

    fixes

--- front/shared/components/initial/initial-section.tsx
+++ front/shared/components/initial/initial-section.tsx
@@ -16,7 +16,7 @@ export const InitialSection: React.FC<Props> = ({ title, cards }) => {
   return (
     <div className="w-full py-16 px-4 bg-ui-dark/95 text-white flex flex-col items-center gap-y-12">
       <h2 className="font-bold text-4xl text-center">{title}</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl w-full">
         {cards.map((card, index) => (
           <InitialCard
             key={index}
